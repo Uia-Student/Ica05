61ff9d11b11c08aab58622e52902e1013f7e78a7
uia-student <TestMail@example.com>
2017-04-04 13:12:11 +0200

    Golang liker ikke unicode

--- httpserv.go
+++ httpserv.go
@@ -7,10 +7,16 @@ import (
 var Port string = ":8001" // lytte port
 func main() {
 fmt.Println("Starting server on port", Port) // debug
-http.HandleFunc("/",handler) //Vær gang en client spør etter "/" på serveren, blir de tatt til funksjonen handler.
+http.HandleFunc("/", handler) 
+http.HandleFunc("/testfunc",testfunc)
 http.ListenAndServe(Port, nil) // starter serveren
 }
 func handler(w http.ResponseWriter, r *http.Request) {
 fmt.Println("Client tried to request /.") //debug
 fmt.Fprintf(w, "Hi there, I Love %s!", r.URL.Path[1:]) // Vi skriver dette til "w" som er http.Responewriter.
+}
+
+func testfunc(w http.ResponseWriter, r *http.Request) {
+fmt.Println("Client tried to request /testfunc") //debug
+fmt.Fprintf(w, "test func") // Vi skriver dette til "w" som er http.Responewriter.
 }
