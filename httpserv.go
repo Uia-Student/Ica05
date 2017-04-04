15bec08da438e86b44ed20b6cbf9e0e9e2fbd76e
uia-student <TestMail@example.com>
2017-04-04 14:18:51 +0200

    Web stuff

--- httpserv.go
+++ httpserv.go
@@ -27,8 +27,8 @@ if err != nil {
 subOpts := geddit.ListingOptions{ //reddit settings
 		Limit: 10,
 	}
-fmt.Println(o.Frontpage(geddit.DefaultPopularity, subOpts))
-
+fpage, _ := o.Frontpage(geddit.DefaultPopularity, subOpts)
+fmt.Println(fpage)
 fmt.Println("Starting server on port", Port) // debug
 http.HandleFunc("/", handler) 
 http.HandleFunc("/testfunc",testfunc)
@@ -40,6 +40,26 @@ fmt.Fprintf(w, "Hi there, I Love %s!", r.URL.Path[1:]) // Vi skriver dette til "
 }
 
 func testfunc(w http.ResponseWriter, r *http.Request) {
-fmt.Println("Client tried to request /testfunc") //debug
-//fmt.Fprintf(w, o.Frontpage(geddit.DefaultPopularity, subOpts)) // Vi skriver dette til "w" som er http.Responewriter.
+fmt.Println("Litt oauth stuff...")
+o, err := geddit.NewOAuthSession(
+    "JjJUDNL57-hisw",
+    "ZcD6YBUPJfh1ob1iu3WMbcp_H5g",
+    "http://none",
+    "http://www.google.com",
+)
+if err != nil {
+    log.Fatal(err)
+}
+fmt.Println("Logger inn")
+err = o.LoginAuth("uiaica05", "uiaica05123")
+if err != nil {
+    log.Fatal(err)
+}
+subOpts := geddit.ListingOptions{ //reddit settings
+		Limit: 10,
+	}
+fpage, _ := o.Frontpage(geddit.DefaultPopularity, subOpts)
+for _, s := range fpage {
+		fmt.Fprintf(w,"Title: %s\nAuthor: %s\n\n", s.Title, s.Author)
+	}
 }
--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Kjører go filen
-set GOPATH="C:\Users\Feiten\Desktop\Skole\ica05"
+set GOPATH=C:\Users\Feiten\Desktop\Skole\ica05
 go run httpserv.go 
 pause > nul
