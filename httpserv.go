c2605dbefc086bd4be4c27cb2cbc078f364386d0
uia-student <TestMail@example.com>
2017-04-04 16:49:38 +0200

    Cleanup

--- httpserv.go
+++ httpserv.go
@@ -52,10 +52,7 @@ func main() {
 	//fpage, _ := o.Frontpage(geddit.DefaultPopularity, subOpts)
 	fmt.Println("Registring handlers") // debug
 	http.HandleFunc("/",personalHandler)
-	http.HandleFunc("/welcome", frontpageHandler)
-	http.HandleFunc("/reddit", redditHandler)
-	http.HandleFunc("/news", worldnewsHandler)
-	http.HandleFunc("/norge", norgeHandler)
+	
 		fmt.Println("Starting server on port", Port) // debug
 	http.ListenAndServe(Port, nil) // starter serveren
 }
@@ -63,15 +60,37 @@ func frontpageHandler(w http.ResponseWriter, r *http.Request) {           //debu
 	fmt.Fprintf(w, "Welcome to Nerds With Attitude's front page. Avaible links:")
 }
 func personalHandler(w http.ResponseWriter, r *http.Request) {
+
     title := r.URL.Path[len("/"):]
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
     }
     t, _ := template.ParseFiles("view.html")
-    t.Execute(w, p)
+	o, err := geddit.NewOAuthSession(
+		"JjJUDNL57-hisw",
+		"ZcD6YBUPJfh1ob1iu3WMbcp_H5g",
+		"http://none",
+		"http://www.google.com",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = o.LoginAuth("uiaica05", "uiaica05123")
+	if err != nil {
+		log.Fatal(err)
+	}
+	subOpts := geddit.ListingOptions{ //reddit settings
+		Limit: 20,
+	}
+	fpage, _ := o.SubredditSubmissions(p.Title, geddit.DefaultPopularity, subOpts)
+	 t.Execute(w, p)
+	for _, s := range fpage {
+		fmt.Fprintf(w,"<br>Title: %s<br>Author: %s<br>Comments: %v<br>Points: %v<br>URL: <a href=\x22%s\x22>Link</a><br>Reddit URL: <a href=\x22https://www.reddit.com/%s\x22>Click here</a><br><br>", s.Title, s.Author, s.NumComments, s.Score, s.URL,s.Permalink)
+	}
+	
 }
-
+/* 
 func redditHandler(w http.ResponseWriter, r *http.Request) {
 	o, err := geddit.NewOAuthSession(
 		"JjJUDNL57-hisw",
@@ -139,5 +158,5 @@ func norgeHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Title: %s\nAuthor: %s\nComments: %v\nPoints: %v\n\n", s.Title, s.Author, s.NumComments, s.Score)
 	}
 }
-
+*/ 
 
